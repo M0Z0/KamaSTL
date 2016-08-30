590662c21875f57c5371d53acdeccdd0a57d1650
makaiupdate <mkmk19900215@163.com>
2016-08-30 18:24:02 +0800

    类型特性

--- HandySTL/HandySTL/Alloc.h
+++ HandySTL/HandySTL/Alloc.h
@@ -1,4 +1,14 @@
 #ifndef __H_ALLOC__
 #define __H_ALLOC__
 
+#include <new.h>
+
+namespace HandySTL{
+
+	template <class T1, class T2>
+	inline void construct(T1 *p, const T2 &value) {
+		new (p)T1(value); //placement new;½ö¹¹Ôì
+	}
+}
+
 #endif
