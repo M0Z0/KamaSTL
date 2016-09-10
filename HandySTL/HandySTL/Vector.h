bdd031107b14ac7a71fe5ff324ea59219b0989a8
makaiupdate <mkmk19900215@163.com>
2016-09-10 21:46:33 +0800

    up

--- HandySTL/HandySTL/Vector.h
+++ HandySTL/HandySTL/Vector.h
@@ -17,22 +17,34 @@ namespace HandySTL{
 		typedef ptrdiff_t difference_type;
 
 	private:
-		pointer start;
-		pointer finish;
-		pointer end_of_storage;
+		pointer _start;
+		pointer _finish;
+		pointer _end_of_storage;
 
 		typedef Alloc dataAllocator;
 	public:
 		//构造、拷贝构造、析构
-		vector() :start(nullptr), finish(nullptr), end_of_storage(nullptr) {}
+		vector() :_start(nullptr), _finish(nullptr), _end_of_storage(nullptr) {}
 		explicit vector(const size_type n);
 		explicit vector(const size_type n, value_type& value);
 		vector(const vector&);
-		vector(vector&&);
+		vector(vector&&); //move construct
+		~vector();
 		vector& operator=(const vector&);
 		vector& operator=(vector&&);
 
-		difference_type capacity() const { return end_of_storage - start; }
+		//容量相关
+		difference_type size() const { return _finish - _start; }
+		difference_type capacity() const { return _end_of_storage - _start; }
+		bool empty() { return (_start == _finish); }
+
+		//迭代器相关
+		iterator begin() { return (_start); }
+		iterator begin() const { return (_start); }
+		iterator end() { return (_finish); }
+		iterator end() const { return (_finish); }
+
+		void push_back(const value_type& value);
 
 	private:
 		void destroyAndDeallocateAll();
--- HandySTL/HandySTL/impl/Vector.impl.h
+++ HandySTL/HandySTL/impl/Vector.impl.h
@@ -18,21 +18,26 @@ namespace HandySTL{
 
 	template <class T, class Alloc>
 	vector<T, Alloc>::vector(const vector &v) {
-		allocate_and_copy(v.start, v.finish);
+		allocate_and_copy(v._start, v._finish);
+	}
+
+	template <class T, class Alloc>
+	vector<T, Alloc>::~vector() {
+		destroyAndDeallocateAll;
 	}
 
 	template <class T, class Alloc>
 	vector<T, Alloc>::vector(vector &&v) {
-		this->start = v.start;
-		this->finish = v.finish;
-		this->end_of_storage = v.end_of_storage;
-		v.start = v.finish = v.end_of_storage = nullptr;
+		this->_start = v._start;
+		this->_finish = v._finish;
+		this->_end_of_storage = v._end_of_storage;
+		v._start = v._finish = v._end_of_storage = nullptr;
 	}
 
 	template <class T, class Alloc>
 	vector<T, Alloc>& vector<T, Alloc>::operator=(const vector &v) {
 		if (this != &v)
-			allocate_and_copy(v.start, v.finish);
+			allocate_and_copy(v._start, v._finish);
 		return *this;
 	}
 
@@ -40,34 +45,37 @@ namespace HandySTL{
 	vector<T, Alloc>& vector<T, Alloc>::operator=(vector &&v) {
 		if (this != &v) {
 			destroyAndDeallocateAll();
-			this->start = v.start;
-			this->finish = v.finish;
-			this->end_of_storage = v.end_of_storage;
-			v.start = v.finish = v.end_of_storage = nullptr;
+			this->_start = v._start;
+			this->_finish = v._finish;
+			this->_end_of_storage = v._end_of_storage;
+			v._start = v._finish = v._end_of_storage = nullptr;
 		}
 		return *this;
 	}
 
 	template <class T, class Alloc>
+	void vector<T, Alloc>::push_back(const value_type& value);
+
+	template <class T, class Alloc>
 	void vector<T, Alloc>::allocate_and_fill(const size_type n, const value_type& value) {
-		start = dataAllocator::allocate(n);
-		HandySTL::uninitialized_fill_n(start, n, value);
-		finish = end_of_storage = start + n;
+		_start = dataAllocator::allocate(n);
+		HandySTL::uninitialized_fill_n(_start, n, value);
+		_finish = _end_of_storage = _start + n;
 	}
 
 	template <class T, class Alloc>
 	template <class InputIterator>
 	void vector<T, Alloc>::allocate_and_copy(InputIterator first, InputIterator last) {
-		start = dataAllocator::allocate(n);
-		finish = HandySTL::uninitialized_copy(first, last, start);
-		end_of_storage = finish;
+		_start = dataAllocator::allocate(n);
+		_finish = HandySTL::uninitialized_copy(first, last, _start);
+		_end_of_storage = _finish;
 	}
 
 	template<class T, class Alloc>
 	void vector<T, Alloc>::destroyAndDeallocateAll(){
 		if (capacity() != 0){
-			dataAllocator::destroy(start, finish);
-			dataAllocator::deallocate(start, capacity());
+			dataAllocator::destroy(_start, _finish);
+			dataAllocator::deallocate(_start, capacity());
 		}
 	}
 
