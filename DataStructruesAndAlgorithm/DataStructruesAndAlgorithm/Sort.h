4efcf2f17750b44c0a8824fd8b7c9b93f39b71e5
makaiupdate <mkmk19900215@163.com>
2016-12-07 23:28:39 +0800

    quicksort

--- DataStructruesAndAlgorithm/DataStructruesAndAlgorithm/Sort.h
+++ DataStructruesAndAlgorithm/DataStructruesAndAlgorithm/Sort.h
@@ -148,6 +148,30 @@ namespace SORT
 		return true;
 	}
 
+	void QuickSort(int s[], int l, int r)
+	{
+		if (l < r)
+		{
+			//Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换
+			int i = l, j = r, x = s[l];
+			while (i<j) 
+			{
+				while (i < j&&s[j] >= x)// 从右向左找第一个小于x的数
+					j--;
+				if (l < r)
+					s[i++] = s[j];
+
+				while (i < j&&s[i] <= x)// 从左向右找第一个大于等于x的数
+					i++;
+				if (l < r)
+					s[j--] = s[i];
+			}
+			s[i] = x;//退出时，i等于j。将x填到这个坑中。 
+			QuickSort(s, l, i - 1);// 递归调用
+			QuickSort(s, i+1, r);
+		}
+	}
+
 }
 
 
